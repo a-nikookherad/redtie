577f7c366e9492d75b5b164bbc0c1881756457ac
nikookherad <nikokherad@gmail.com>
2019-03-30 18:52:52 +0430

    fix push

--- route/web.php
+++ route/web.php
@@ -3,3 +3,4 @@
 $route->setRoute('home','homeController@index');
 $route->setRoute('/','defaults\homeController@home');
 //	$route->setRoute('name/{name}/family/{family}','App\Controllers\homeController@home');
+
